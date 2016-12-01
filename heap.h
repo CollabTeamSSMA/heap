822d78e25785f20caead479738fb31436809afbe
CSCIMattAustin <matgaust@ut.utm.edu>
2016-12-01 12:33:13 -0600

    Done

--- heap.h
+++ heap.h
@@ -6,19 +6,109 @@
 #include <iostream>
 using namespace std;
 
-//template is there is time
+template <class T>
 class heap{
  private:
   void remove_max_helper(unsigned index);
   void insert_helper(unsigned index);
-  vector<int> buffer;
+  vector<T> buffer;
  public:
   heap();
   int size();
-  void insert(int value);
+  void insert(T value);
   void remove_max();
-  int max();
+  T max();
   void print();
-  void swap(int & First_num, int & Second_num);
+  void swap(T & First_num, T & Second_num);
 };
 #endif //HEAP_H
+
+template <class T>
+heap<T>::heap(){
+  //T position_zero = NULL;
+  //buffer.push_back(position_zero);
+  buffer.resize(1);
+}
+
+template <class T>
+int heap<T>::size(){
+  return buffer.size()-1;
+}
+
+template <class T>
+void heap<T>::insert(T value){
+  buffer.push_back(value);
+  insert_helper(buffer.size()-1);
+
+}
+
+template <class T>
+void heap<T>::insert_helper(unsigned index){
+  if (index == 1)
+    return;
+  else if (buffer[index] >= buffer[index/2]){
+    swap(buffer[index], buffer[index/2]);
+    insert_helper(index/2);
+  }
+}
+
+template <class T>
+void heap<T>::print(){
+  for(unsigned i = 1; i < buffer.size(); i++)
+    cout << buffer[i] << " ";
+  cout << endl;
+}
+template <typename T>
+T heap<T>::max(){
+  return buffer[1];
+}
+
+template <class T>
+void heap<T>::remove_max(){
+  swap(buffer[1], buffer[buffer.size()-1]);
+  buffer.pop_back();
+  if(buffer.size() > 2)
+    remove_max_helper(1);
+}
+
+template <class T>
+void heap<T>::remove_max_helper(unsigned index){
+  if(buffer[index] < buffer[index*2] && index*2+1 >= buffer.size()-1){
+    swap(buffer[index], buffer[index*2]);
+  }
+	 
+    if(buffer[index] < buffer[index*2] && buffer[index] < buffer[(index*2)+1]){
+      if(buffer[(index*2)+1] < buffer[index*2] && (index*2) < buffer.size()){
+    	cout << buffer[index] << " swap with " << buffer[index*2];
+    	swap(buffer[index], buffer[index*2]);
+    	remove_max_helper(index+1);
+	insert_helper(index);
+      }
+      else if(buffer[(index*2)+1] >= buffer[index*2] && (index*2)+1 < buffer.size()){
+    	cout << buffer[index] << " swap with " << buffer[index*2+1] << endl;
+    	swap(buffer[index], buffer[(index*2)+1]);
+    	remove_max_helper(index+1);
+	insert_helper(index);
+      }
+    }
+    else if (buffer[index] < buffer[index*2] && (index*2) < buffer.size() && buffer[index*2+1] <= buffer[index]){
+      cout << buffer[index] << " swap with " << buffer[index*2] << endl;
+      swap(buffer[index], buffer[index*2]);
+      remove_max_helper(index+1);
+      insert_helper(index);
+    } 
+    else if(buffer[index] < buffer[(index*2)+1] && (index*2)+1 < buffer.size()  && buffer[index*2] <= buffer[index]){
+      cout << buffer[index] << " swap with " << buffer[index*2+1] << endl;
+      swap(buffer[index], buffer[(index*2)+1]);
+      remove_max_helper(index+1);
+      insert_helper(index);
+    }
+    
+}
+
+template <class T>
+void heap<T>::swap(T &First_num, T &Second_num){
+  T temp = First_num;
+  First_num = Second_num;
+  Second_num = temp;
+}
--- main.cpp
+++ main.cpp
@@ -9,42 +9,33 @@
 using namespace std;
 
 void print_welcome();
-
+void print_type_query();
+template<typename T>
+void driver(T value, heap<T> h);
 int main(){
-  //const int SOME_NUMBER = 10;
-  heap h;
-  int value;
-  char answer;
-  print_welcome();//welcome screen
-  cin >> answer;//priming read
-  while(answer != 'q'){
-    if (answer == '1'){//inserts a given valueand data
-      cout << "Please enter the value you would like to insert: ";
-      cin >> value;
-      h.insert(value);
-      // for(int i=0; i<SOME_NUMBER; i++){
-      // 	int r = rand()%100;
-      // 	h.insert(r);
-      // 	h.print();
-      // }
-     }
-    else if(answer == '2')//prints the heap
-      h.print();
-    else if(answer == '3')
-      cout << "The max value in the heap is: " << h.max() << endl;
-    else if(answer == '4'){
-      h.remove_max();
-       // while(h.size() > 0) {
-       // 	cout << h.max() << endl;
-       // 	h.print();
-       // 	h.remove_max();
-       // }
-    }
-    else if(answer == '5')//reads from a file and inserts into the tree
-      cout << "The size of the heap is " << h.size() << endl;
-    
-    print_welcome();//welcome screen
-    cin >> answer;//last read
+  heap<int> h;
+  char type;
+  print_type_query();
+  cin >> type;
+  if (type == '1'){
+    heap<string> h;
+    string value = "";
+    driver(value, h);
+  }
+  else if (type == '2'){
+    heap<int> h;
+    int value = 0;
+    driver(value, h);
+  }
+  else if (type == '3'){
+    heap<float> h;
+    float value = 0;
+    driver(value, h);
+  }
+  else {
+    heap<char> h;
+    char value = '\0';
+    driver(value, h);
   }
   return 0;
 }
@@ -68,4 +59,45 @@ void print_welcome(){
   for (int i = 0; i<54; i++)
     cout << "-";//bottom of the box
   cout << "\n?";
+}
+void print_type_query(){
+  for (int i = 0; i<29; i++)
+    cout << "-";//top of the box
+    for(int i = 0; i<28; i++)//empty line
+    cout << " ";
+  //options
+    cout << "|\n|<<Enter the type you want>>|" 
+	 << "\n|<<(1)->string>>>>>>>>>>>>>>|"
+	 << "\n|<<(2)->int>>>>>>>>>>>>>>>>>|"
+	 << "\n|<<(3)->float>>>>>>>>>>>>>>>|"
+	 << "\n|<<(4)->char>>>>>>>>>>>>>>>>|"
+	 << endl;
+    for (int i = 0; i<29; i++)
+      cout << "-";//bottom of the box
+    cout << "\n?";
+}
+template<typename T>
+void driver(T value, heap<T> h){
+  char answer; 
+  print_welcome();//welcome screen
+  cin >> answer;//priming read
+  while(answer != 'q'){
+    if (answer == '1'){//inserts a given valueand data
+      cout << "Please enter the value you would like to insert: ";
+      cin >> value;
+      h.insert(value);
+    }
+    else if(answer == '2')//prints the heap
+      h.print();
+    else if(answer == '3')
+      cout << "The max value in the heap is: " << h.max() << endl;
+    else if(answer == '4'){
+      h.remove_max();
+    }
+    else if(answer == '5')//reads from a file and inserts into the tree
+      cout << "The size of the heap is " << h.size() << endl;
+    
+    print_welcome();//welcome screen
+    cin >> answer;//last read
+  }
 }
